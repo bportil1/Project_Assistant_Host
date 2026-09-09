b3dfded550f4179bccb94254944231d370ee9a67
Bryan Portillo <bryanportillo49@yahoo.com>
2026-09-09 07:56:26 -0600

    modified:   pah/components.py

--- pah/components.py
+++ pah/components.py
@@ -140,7 +140,7 @@ GIT_COMPONENTS = (
         label="HSQA_DBN",
         path="modules/hsqa_dbn",
         repository_url="git@github.com:bportil1/HSQA_DBN.git",
-        branch="refactor",
+        default_branch="refactor",
     ),
     GitComponent(
         key="tech_documents",
